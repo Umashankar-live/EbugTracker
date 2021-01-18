bdf3fb3b45b7c771616e7101291c80af000b8478
Uma Shankar <56968738+Umashankar-live@users.noreply.github.com>
2021-01-18 17:38:46 +0530

    Created Microservices

---
+++ Back-end/Ticket-Microservice/src/test/java/com/cg/ebug/TicketMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.ebug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
