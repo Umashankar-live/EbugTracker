f606fc3fa8f827887648b13cc2b820353b37c22c
amulyareddy23 <ammu2315reddy@gmail.com>
2021-02-15 10:13:03 +0530

    controller tests

--- Back-end/Customer-Microservice/src/test/java/com/cg/ebug/CustomerMicroserviceApplicationTests.java
+++ Back-end/Customer-Microservice/src/test/java/com/cg/ebug/CustomerMicroserviceApplicationTests.java
@@ -3,6 +3,7 @@ package com.cg.ebug;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -11,22 +12,28 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import com.cg.ebug.controller.CustomerController;
 import com.cg.ebug.dao.TicketRepository;
 import com.cg.ebug.entity.TicketTable;
 import com.cg.ebug.service.ICustomerService;
-
-
-
 
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 class CustomerMicroserviceApplicationTests {
 
+	@Autowired
+	private CustomerController controller;
 
 	@Autowired
 	private ICustomerService service;
@@ -34,7 +41,7 @@ class CustomerMicroserviceApplicationTests {
 	@MockBean
 	private TicketRepository ticketRepository;
 
-
+	Logger logger = LoggerFactory.getLogger(CustomerMicroserviceApplicationTests.class);
 	@Test
 	void getTicketsByCustIdTest() {
 
@@ -57,6 +64,31 @@ class CustomerMicroserviceApplicationTests {
 		assertNotNull(expectedList);
 		assertFalse(expectedList.isEmpty());
 
+	}
+
+	@Test
+	void getTicketsByCustIdControllerTest(){
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+		final Long custId = 1L ;
+		byte[] bytes = new byte[120];
+		List<TicketTable> ticketsList = new ArrayList<TicketTable>();
+
+		final TicketTable t1 = new TicketTable(1L, 1L, "title", "description", "solution", "name", "type", bytes,
+				"open", "low", false, 1001, "employeeName", false, "projectName");
+
+		final TicketTable t2 = new TicketTable(2L, 1L, "title", "description", "solution", "name", "type", bytes,
+				"open", "low", false, 1001, "employeeName", false, "projectName");
+
+		ticketsList.add(t1);
+		ticketsList.add(t2);
+
+		when(ticketRepository.findTicketsByCustId(custId)).thenReturn(ticketsList);
+		when(service.getAllTicketsByCustomerId(custId)).thenReturn(ticketsList);
+
+		List<TicketTable> responseList = controller.getImage(custId);
+
+		assertEquals(2, responseList.size());
 
 	}
 
@@ -76,6 +108,22 @@ class CustomerMicroserviceApplicationTests {
 	}
 
 	@Test
+	void getTicketByIdControllerTest(){
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+
+		byte[] bytes = new byte[120];
+		TicketTable ticket = new TicketTable(1L, 1001, "title", "description", "solution", "name", "type", bytes,
+				"open", "low", false, 1001, "employeeName", false, "projectName");
+
+		when(ticketRepository.save(ticket)).thenReturn(ticket);
+		when(ticketRepository.findTicketById(1L)).thenReturn(Optional.of(ticket));
+
+		assertEquals(ticket.getId(), controller.viewTicketById(1L).getTicketTable().getId());
+
+	}
+
+	@Test
 	void updateTicketByCustTest() {
 		byte[] bytes = new byte[120];
 		final TicketTable ticket = new TicketTable(1L, 1001, "title", "description", "solution", "name", "type", bytes,
@@ -86,6 +134,27 @@ class CustomerMicroserviceApplicationTests {
 		final TicketTable expected = service.updateTicketByCustomer(ticket);
 		assertThat(expected).isNotNull();
 	}
+
+	@Test
+	void updateTicketByCustomerControllerTest() {
+		try {
+			MockHttpServletRequest request = new MockHttpServletRequest();
+			RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+
+			byte[] bytes = new byte[120];
+			TicketTable ticket = new TicketTable(1L, 1001, "title", "description", "solution", "name", "type", bytes,
+					"open", "low", false, 1001, "employeeName", false, "projectName");
+
+			when(ticketRepository.save(ticket)).thenReturn(ticket);
+			when(ticketRepository.findById(1L)).thenReturn(Optional.of(ticket));
+
+			ResponseEntity<TicketTable> responseEntity = controller.updateTicketsByCustomer(ticket, 1L);
+			assertEquals(ticket.getId(), responseEntity.getBody().getId());
+		} catch (Exception e) {
+			logger.info(e.getMessage());
+		}
+	}
+
 
 	@Test
 	void raiseTicketTest() {
